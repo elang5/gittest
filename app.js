c5325ec29bbe54eba75ebae2741a79b6f818765b
Elan Green <green.elan5@gmail.com>
2019-01-30 18:48:22 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const elan = {
+    age: 23,
+    dateOfBirth: "May 8th, 1995",
+    birthplace: "Oregon City, OR",
+    hobby: "cello"
+}
+
+const elanArr = Object.keys(elan);
+
+console.log(elanArr);
